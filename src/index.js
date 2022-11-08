c9da48e3cefd7c7b88240c4788c60b0bf0be0093
vivekky <vivekky57@gmail.com>
2022-11-08 23:54:27 +0530

    Socket.io Events

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+let count = 0;
+//server(emit) -> client (receive) - countUpdated
+//client(emit) -> server(receive) - increment
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("countUpdated", count);
+  socket.on("increment", () => {
+    count++;
+    //socket.emit('countUpdated',count)
+    io.emit("countUpdated", count);
+  });
+});
+server.listen(port, () => {
   console.log("Server is up on port " + port);
 });
